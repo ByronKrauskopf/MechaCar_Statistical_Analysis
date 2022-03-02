f018b8fc1ae941f7809fb0b94931a6dfbf0c278f
Byron Krauskopf <byronkrauskopf@gmail.com>
2022-03-02 10:51:14 -0500

    upload images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,10 +1,17 @@
 library(dplyr) #load dplyr package
 
+#Deliverable 1
+
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F) #import MPG csv data as dataframe
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg) #multiple linear regression
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)) #summary statistics
 
+#Deliverable 2
+
 Suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F) #import suspension csv data as dataframe
 
+total_summary <- Suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI)) #summary dataframe
+
+lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') #create lot summaries
